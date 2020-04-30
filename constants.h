dc736aff641b58175749fbe25aaeea5ca9b5bb17
Mathieu Philippart <philippartmathieu@gmail.com>
2020-05-01 00:15:13 +0200

    Fixed M_PI for windows

--- constants.h
+++ constants.h
@@ -10,15 +10,15 @@ using namespace std::literals::complex_literals;
 // To avoid to type std:: before each complex
 using namespace std;
 
+#ifndef M_PI
+#define M_PI    3.14159265358979323846  // pi
+#define M_PI_2  1.57079632679489661923  // pi/2
+#endif
+
 const double LIGHT_SPEED = 299792458.0;     // [m/s]
 const double EPSILON_0   = 8.85418782e-12;  // [F/m]
 const double MU_0        = 12.566370614e-7; // [T m/A]
 
 const double Z_0 = 120 * M_PI;  // [Ohm]
-
-#ifndef M_PI
-#define M_PI    3.14159265358979323846  // pi
-#define M_PI_2  1.57079632679489661923  // pi/2
-#endif
 
 #endif // CONSTANTS_H
